c76366d512017738e9ac49c41789f909c5a07577
Lisa Rodriguez <105234340+lisa12rdgz@users.noreply.github.com>
2025-05-01 23:19:59 -0600

    deteccion por colores

--- kalmanfilterapp.py
+++ kalmanfilterapp.py
@@ -40,7 +40,38 @@ balls = [] # arreglo para porder detectar mas de un objecto a la vez
 cap = cv2.VideoCapture("final.mp4") # video a analizar
 
 while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
 
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # rangos amarillo
+    lower_yellow = np.array([15, 100, 100])#10 - 50 , 15-43, 
+    upper_yellow = np.array([43, 255, 255])
+
+    # rangos naranja
+    lower_orange = np.array([7, 130, 130])
+    upper_orange = np.array([25, 205, 205])
+
+    # rsngos blancos
+    lower_white = np.array([0, 0, 200])
+    upper_white = np.array([180, 50, 255])
+
+    # mascaras
+    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+    mask_white = cv2.inRange(hsv, lower_white, upper_white)
+
+    # Máscara combinada
+    mask = cv2.bitwise_or(mask_yellow, mask_orange)
+    mask = cv2.bitwise_or(mask, mask_white)
+
+    mask = cv2.erode(mask, None, iterations=4)
+    mask = cv2.dilate(mask, None, iterations=6)
+
+    cv2.imshow('Mascara HSV', mask) # mostrar el filtro de colores
 
 cap.release()
 cv2.destroyAllWindows()
